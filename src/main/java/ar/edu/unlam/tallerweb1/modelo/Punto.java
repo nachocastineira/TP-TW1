15298df806cbc0773b39bcd0336aba7c5cb29a9b
nachocastineira <nacho2.0804@gmail.com>
2017-09-05 14:31:32 -0300

    clases y tests limpios agregados

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Punto.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Punto {
+
+}
---
+++ src/test/java/ar/edu/unlam/tallerweb1/modelo/Tp1Test.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tp1Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
