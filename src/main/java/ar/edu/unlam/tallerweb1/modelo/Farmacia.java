228afc45d5959db60644c261754ac2ee9db73c6a
ferezecarr <ferezecarr@gmail.com>
2017-09-07 14:15:04 -0300

    subo la farmacia y el punto

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Farmacia.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Farmacia.java
@@ -1,8 +1,89 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Farmacia {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idFarmacia;
+	private String nombreFarmacia;
+	private String telefonoFarmacia;
+	private String diaDeTurno;
+	
+	@ManyToOne (fetch = FetchType.LAZY)
+	@JoinColumn(name="idDireccion")
+	private Direccion direccion;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="idPunto")
+	private Punto geoLocalizacion;
+	
+	public Farmacia(){}
+	
+	public Farmacia(String nombreFarmacia , String telefonoFarmacia){
+		this.nombreFarmacia = nombreFarmacia;
+		this.telefonoFarmacia = telefonoFarmacia;
+	}
+
+	public Long getIdFarmacia() {
+		return idFarmacia;
+	}
+
+	public void setIdFarmacia(Long idFarmacia) {
+		this.idFarmacia = idFarmacia;
+	}
+
+	public String getNombreFarmacia() {
+		return nombreFarmacia;
+	}
+
+	public void setNombreFarmacia(String nombreFarmacia) {
+		this.nombreFarmacia = nombreFarmacia;
+	}
+
+	public String getTelefonoFarmacia() {
+		return telefonoFarmacia;
+	}
+
+	public void setTelefonoFarmacia(String telefonoFarmacia) {
+		this.telefonoFarmacia = telefonoFarmacia;
+	}
+
+	public String getDiaDeTurno() {
+		return diaDeTurno;
+	}
+
+	public void setDiaDeTurno(String diaDeTurno) {
+		this.diaDeTurno = diaDeTurno;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	public Punto getGeoLocalizacion() {
+		return geoLocalizacion;
+	}
+
+	public void setGeoLocalizacion(Punto geoLocalizacion) {
+		this.geoLocalizacion = geoLocalizacion;
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Punto.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Punto.java
@@ -1,8 +1,57 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Punto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idPunto;
+	private String latitud;
+	private String longitud;
+	
+	@OneToMany(mappedBy="farmacia" , cascade=CascadeType.ALL)
+	private List<Punto> puntos = new ArrayList();
+	
+	public Punto(){}
+	
+	public Punto(String latitud , String longitud){
+		this.latitud = latitud;
+		this.longitud = longitud;
+	}
+	
+	public Long getIdPunto() {
+		return idPunto;
+	}
+	
+	public void setIdPunto(Long idPunto) {
+		this.idPunto = idPunto;
+	}
+	
+	public String getLatitud() {
+		return latitud;
+	}
+	
+	public void setLatitud(String latitud) {
+		this.latitud = latitud;
+	}
+	
+	public String getLongitud() {
+		return longitud;
+	}
+	
+	public void setLongitud(String longitud) {
+		this.longitud = longitud;
+	}
+	
+	
 }
