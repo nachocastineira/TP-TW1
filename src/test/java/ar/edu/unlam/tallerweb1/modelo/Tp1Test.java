0f27be466deda5244650a3086b06a261686a7744
ferezecarr <ferezecarr@gmail.com>
2017-09-07 14:31:13 -0300

    subo el primer test

--- src/test/java/ar/edu/unlam/tallerweb1/modelo/Tp1Test.java
+++ src/test/java/ar/edu/unlam/tallerweb1/modelo/Tp1Test.java
@@ -2,13 +2,49 @@ package ar.edu.unlam.tallerweb1.modelo;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-public class Tp1Test {
+import ar.edu.unlam.tallerweb1.SpringTest;
 
+public class Tp1Test extends SpringTest {
+
+	@SuppressWarnings("unchecked")
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Transactional
+	@Rollback(true)
+	public void testQueBuscaTodasLasFarmaciasDeTurnoLosDiasMartes() {
+		
+		Farmacia primerFarmacia = new Farmacia();
+		primerFarmacia.setDiaDeTurno("Martes");
+		
+		Farmacia segundaFarmacia = new Farmacia();
+		segundaFarmacia.setDiaDeTurno("Lunes");
+		
+		Farmacia tercerFarmacia = new Farmacia();
+		tercerFarmacia.setDiaDeTurno("Miercoles");
+		
+		getSession().save(primerFarmacia);
+		getSession().save(segundaFarmacia);
+		getSession().save(tercerFarmacia);
+		
+		Session miSesion = getSession();
+		
+		List<Farmacia> miFarmacia;
+		
+		//--- 1. Buscar todas las farmacias de turno los dias martes ---//
+		
+		miFarmacia = miSesion.createCriteria(Farmacia.class).add(Restrictions.lt("diaDeTurno", "martes")).list();
+		
+		assertThat(miFarmacia.get(0).getDiaDeTurno());
+		assertThat(miFarmacia).hasSize(1);
 	}
 
 }
